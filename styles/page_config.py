988f84deb5d34e0a437c272f3d224522c0ab8981
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2026-04-09 17:24:25 +0800

    Refactor page configuration and styling for ContentClouder application

--- styles/page_config.py
+++ styles/page_config.py
@@ -1,5 +1,28 @@
 import streamlit as st
+import os
 
 def page_config():
-    st.set_page_config("ContentClouder", page_icon="☁️", layout="wide")
-    st.title("ContentClouder")
+    st.set_page_config(page_title="ContentClouder", page_icon="☁️", layout="wide")
+    css_path = os.path.join("styles", "style.css")
+    if os.path.exists(css_path):
+        with open(css_path) as file:
+            st.markdown(f"<style>{file.read()}</style>", unsafe_allow_html=True)
+
+    st.markdown("""
+        <h1>
+            <span class="title-accent">Content</span><span class="title-main">Clouder</span>
+        </h1>
+        """, 
+        unsafe_allow_html=True
+    )
+    
+    st.markdown(
+        """
+        <div class="app-description">
+            ContentClouder is a <b>Python-powered</b> web application that transforms lengthy 
+            YouTube videos into intuitive word cloud visualizations, allowing users to grasp the 
+            core themes of a transcript in seconds.
+        </div>
+        """, 
+        unsafe_allow_html=True
+    )
