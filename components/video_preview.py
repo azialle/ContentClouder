c8562520807a546389a48d4dfa8ef409d4c06280
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2026-04-10 16:41:36 +0800

    Enhance video preview layout and styling

--- components/video_preview.py
+++ components/video_preview.py
@@ -7,20 +7,28 @@ class YTVideoPreview(YoutubeFetcher):
 
     def preview(self, generate_btn):
         thumbnail_url = self.video_thumbnail()
+        metadata = self.video_metadata()
 
         if thumbnail_url and not generate_btn:
-            st.subheader("Video Preview")
-            with st.container(border=True):
-                col1, col2 = st.columns(2)
+            st.subheader("Video Preview", text_alignment="center")
+            left_gap, center_col, right_gap = st.columns([1, 5, 1])
+            with center_col:
+                with st.container(border=True, width="stretch"):
+                    col1, col2 = st.columns([1, 1.5], gap="medium")
                 with col1:
-                    st.image(thumbnail_url, width="stretch", link=self.url)
+                    st.image(thumbnail_url, width="stretch")
                 with col2:
-                    metadata = self.video_metadata()
                     if metadata:
-                        st.subheader(metadata["title"])
-                        st.write(f"**Channel:** {metadata["channel"]}")
-                        st.write(f"**Published Date**: {metadata["published_date"]}")
-                        st.write(f"**Duration:** {metadata["duration"]}")
-                        st.write(f"**Views**: {metadata["views"]}")
+                        st.markdown(f"""
+                        <div class="preview-text-column">
+                            <div class="preview-title">{metadata["title"]}</div>
+                            <div class="preview-metadata">
+                                <b>Channel:</b> {metadata["channel"]} <br>
+                                <b>Duration:</b> {metadata["duration"]} <br>
+                                <b>Views:</b> {metadata["views"]:,} views <br>
+                                <b>Published:</b> {metadata["published_date"]}
+                            </div>
+                        </div>
+                    """, unsafe_allow_html=True)
                     else:
                         st.warning("Metadata unavailable for this video.")
