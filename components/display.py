08570a01854320227acfcef588b543bfe4a9c43a
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2026-04-27 21:58:56 +0800

    Refactor video_preview function to adjust column proportions for better layout

--- components/display.py
+++ components/display.py
@@ -22,7 +22,7 @@ def url_input():
 
 def video_preview(thumbnail_url, metadata):
     with st.container(border=True, width="stretch"):
-        col1, col2 = st.columns([1, 1.5])
+        col1, col2 = st.columns(2)
         with col1:
             st.image(thumbnail_url)
         with col2:
